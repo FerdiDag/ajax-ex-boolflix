33e0ff9c7528bf1dead84b9e11116c17dc94848b
Ferdinando <ferdi.dagostino@gmail.com>
2020-05-25 12:47:26 +0200

    aggiunta di funzioni

--- main.js
+++ main.js
@@ -16,30 +16,7 @@ $(document).ready(function() {
             alert('Inserisci un titolo valido');
         } else {
             //inizio chiamata ajax
-            $.ajax({
-                'url': 'https://api.themoviedb.org/3/search/movie',
-                'method': 'GET',
-                'data': {
-                    'api_key': 'd2f2e36584ccedbe3c1a6c903ec79afb',
-                    'query': messaggio,
-                    'language': 'it',
-                },
-                success: function(data) {
-                    // mi viene restituito un array come risultato e lo salvo in una variabile
-                    var array_risultati = data.results;
-                    console.log(array_risultati)
-                    if (array_risultati.length == 0) {
-                        alert('Nessun risultato trovato');
-                    }
-                    //funzione che stampa in pagina i risultati ottenuti
-                    generaCard(array_risultati);
-
-
-                },
-                error: function() {
-                    console.log('errore');
-                }
-            });
+            ricercaFilm(messaggio);
             //fine chiamata ajax
         }
 
@@ -59,27 +36,64 @@ $(document).ready(function() {
     });
 
 
+
+
+    function ricercaFilm(messaggio) {
+        //inizio chiamata ajax
+        $.ajax({
+            'url': 'https://api.themoviedb.org/3/search/movie',
+            'method': 'GET',
+            'data': {
+                'api_key': 'd2f2e36584ccedbe3c1a6c903ec79afb',
+                'query': messaggio,
+                'language': 'it',
+            },
+            success: function(data) {
+                // mi viene restituito un array come risultato e lo salvo in una variabile
+                var array_risultati = data.results;
+                console.log(array_risultati)
+                if (array_risultati.length == 0) {
+                    alert('Nessun risultato trovato');
+                } else {
+                    //funzione che stampa in pagina i risultati ottenuti
+                    generaCard(array_risultati);
+                }
+
+
+
+
+            },
+            error: function() {
+                console.log('errore');
+            }
+        });
+        //fine chiamata ajax
+    }
     //funzione che stampa in pagina i risultati ottenuti
     function generaCard(array_risultati) {
         for (var i = 0; i < array_risultati.length; i++) {
             var risultato_corrente = array_risultati[i];
-
-            //salvo ogni risultato dell'array in un nuovo oggetto, per poi eventualmente usare handlebars
-            var locandina_film = {
-                'titolo': risultato_corrente.title,
-                'titolo-originale': risultato_corrente.original_title,
-                'lingua': risultato_corrente.original_language,
-                'voto': risultato_corrente.vote_average,
-
-            }
+            scrivi_locandina(risultato_corrente);
 
 
+        }
+    }
+    //funzione che inserisce i dati trovati in un oggetto
+    function scrivi_locandina(risultato_corrente) {
+        //salvo ogni risultato dell'array in un nuovo oggetto, per poi eventualmente usare handlebars
+        var locandina_film = {
+            'titolo': risultato_corrente.title,
+            'titolo-originale': risultato_corrente.original_title,
+            'lingua': risultato_corrente.original_language,
+            'voto': Math.ceil(risultato_corrente.vote_average / 2),
 
-            var html_finale = template_function(locandina_film);
-
-            // appendo in pagina una card con i dati dei film
-            $('.film-container').append(html_finale);
         }
 
+
+
+        var html_finale = template_function(locandina_film);
+
+        // appendo in pagina una card con i dati dei film
+        $('.film-container').append(html_finale);
     }
 });
