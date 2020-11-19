8c1e034a1962ec49c196c6a87b5c40c4f002ce41
Ferdinando <ferdi.dagostino@gmail.com>
2020-11-19 16:26:07 +0100

    push

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
   //recupero il template
-  var template_html = $("#film-template").html();
+  var template_html = $('#film-template').html();
   //preparo la funzione da utilizzare per il template
   var template_function = Handlebars.compile(template_html);
 
   //preparo delle variabili per ajax
-  var api_key = "d2f2e36584ccedbe3c1a6c903ec79afb";
-  var api_url_base = "https://api.themoviedb.org/3/";
-  var stringa_movie = "search/movie";
-  var stringa_tv = "search/tv";
-  var tipoFilm = "Film";
-  var tipoSerieTv = "Serie-tv";
-  var api_img_url_base = "https://image.tmdb.org/t/p/";
-  var dimensione_img = "w342";
-  var lingua = "it";
+  var api_key = 'd2f2e36584ccedbe3c1a6c903ec79afb';
+  var api_url_base = 'https://api.themoviedb.org/3/';
+  var stringa_movie = 'search/movie';
+  var stringa_tv = 'search/tv';
+  var tipoFilm = 'Film';
+  var tipoSerieTv = 'Serie-tv';
+  var api_img_url_base = 'https://image.tmdb.org/t/p/';
+  var dimensione_img = 'w342';
+  var lingua = 'it';
 
   function chiamataAjax(par0, par1, par2) {
     $.ajax({
       url: api_url_base + par1,
-      method: "GET",
+      method: 'GET',
       data: {
         api_key: api_key,
         query: par0,
@@ -27,16 +27,17 @@ $(document).ready(function () {
       success: function (data) {
         // mi viene restituito un array come risultato e lo salvo in una variabile
         var array_risultati = data.results;
-        console.log(array_risultati);
+
         if (array_risultati.length == 0) {
-          alert("Nessun risultato trovato");
+          alert('Nessun risultato trovato');
         } else {
           //funzione che stampa in pagina i risultati ottenuti
           generaCard(array_risultati, par2);
         }
       },
+
       error: function () {
-        console.log("errore");
+        console.log('errore');
       },
     });
     //fine chiamata ajax
@@ -54,14 +55,14 @@ $(document).ready(function () {
     //inizio chiamata ajax
     $.ajax({
       url: url,
-      method: "GET",
+      method: 'GET',
       data: {
         api_key: api_key,
       },
       success: function (data) {
         var array_nomi = [];
         var array_cast = data.cast;
-        console.log(data.cast);
+
         for (var i = 0; i < 5; i++) {
           if (array_cast[i]) {
             var cast_corrente = array_cast[i];
@@ -69,14 +70,12 @@ $(document).ready(function () {
           }
         }
 
-        console.log(array_nomi);
-
         // trova elementi di classe = "cast" con attributo di nome "data-id" che è uguale a id (variabile)
-        $('.cast[data-id="' + id + '"]').append(array_nomi.join(", "));
+        $('.cast[data-id="' + id + '"]').append(array_nomi.join(', '));
       },
 
       error: function () {
-        console.log("errore");
+        console.log('errore');
       },
     });
     //fine chiamata ajax
@@ -86,14 +85,14 @@ $(document).ready(function () {
     //inizio chiamata ajax
     $.ajax({
       url: url2,
-      method: "GET",
+      method: 'GET',
       data: {
         api_key: api_key,
       },
       success: function (data) {
         var array_nomi = [];
         var array_cast = data.genres;
-        console.log(data.genres);
+
         for (var i = 0; i < 5; i++) {
           if (array_cast[i]) {
             var cast_corrente = array_cast[i];
@@ -101,14 +100,12 @@ $(document).ready(function () {
           }
         }
 
-        console.log(array_nomi);
-
         // trova elementi di classe = "genre" con attributo di nome "data-id" che è uguale a id (variabile)
-        $('.genre[data-id="' + id + '"]').append(array_nomi.join(", "));
+        $('.genre[data-id="' + id + '"]').append(array_nomi.join(', '));
       },
 
       error: function () {
-        console.log("errore");
+        console.log('errore');
       },
     });
     //fine chiamata ajax
@@ -130,28 +127,27 @@ $(document).ready(function () {
       id: risultato_corrente.id,
     };
 
-    console.log(locandina_film);
     var html_finale = template_function(locandina_film);
     // appendo in pagina una card con i dati dei film
 
-    $(".film-container").append(html_finale);
+    $('.film-container').append(html_finale);
 
     var url;
-    if (tipo == "Film") {
+    if (tipo == 'Film') {
       url =
-        "https://api.themoviedb.org/3/movie/" +
+        'https://api.themoviedb.org/3/movie/' +
         risultato_corrente.id +
-        "/credits";
-    } else if (tipo == "Serie-tv") {
+        '/credits';
+    } else if (tipo == 'Serie-tv') {
       url =
-        "https://api.themoviedb.org/3/tv/" + risultato_corrente.id + "/credits";
+        'https://api.themoviedb.org/3/tv/' + risultato_corrente.id + '/credits';
     }
 
     var url2;
-    if (tipo == "Film") {
-      url2 = "https://api.themoviedb.org/3/movie/" + risultato_corrente.id;
-    } else if (tipo == "Serie-tv") {
-      url2 = "https://api.themoviedb.org/3/tv/" + risultato_corrente.id;
+    if (tipo == 'Film') {
+      url2 = 'https://api.themoviedb.org/3/movie/' + risultato_corrente.id;
+    } else if (tipo == 'Serie-tv') {
+      url2 = 'https://api.themoviedb.org/3/tv/' + risultato_corrente.id;
     }
 
     stampa_attori(risultato_corrente.id, url);
@@ -160,8 +156,8 @@ $(document).ready(function () {
 
   function voto_stella(voti) {
     var voto = Math.ceil(voti / 2);
-    var stellaFas = "";
-    var stellaFar = "";
+    var stellaFas = '';
+    var stellaFar = '';
     for (var i = 0; i < voto; i++) {
       stellaFas += '<i class="fas fa-star"></i>';
     }
@@ -172,7 +168,7 @@ $(document).ready(function () {
   }
 
   function seleziona_lingua(lingua) {
-    var array_lingue = ["en", "it", "fr", "de", "es"];
+    var array_lingue = ['en', 'it', 'fr', 'de', 'es'];
 
     if (array_lingue.includes(lingua)) {
       return '<img src="flag_' + lingua + '.png"">';
@@ -183,24 +179,24 @@ $(document).ready(function () {
 
   function seleziona_poster(poster) {
     if (!poster) {
-      return "immagine-non-disponibile.png";
+      return 'immagine-non-disponibile.png';
     } else {
       return api_img_url_base + dimensione_img + poster;
     }
   }
 
   //intercetto il click sul bottone submit
-  $("#input-button").click(function () {
+  $('#input-button').click(function () {
     //prendo ciò che è stato scritto dall'utente nella searchbar e lo salvo in una variabile
-    var messaggio = $("#input-text").val();
+    var messaggio = $('#input-text').val();
     //rimuove i risultati ottenuti
-    $(".film-container").html("");
+    $('.film-container').html('');
     //svuota la searchbar dopo aver premuto invio/click
-    $("#input-text").val("");
-    if (!messaggio.replace(/\s/g, "").length) {
-      alert("Inserisci un titolo valido");
+    $('#input-text').val('');
+    if (!messaggio.replace(/\s/g, '').length) {
+      alert('Inserisci un titolo valido');
     } else if (messaggio.length < 2) {
-      alert("Inserisci almeno due caratteri");
+      alert('Inserisci almeno due caratteri');
     } else {
       //inizio chiamata ajax
       chiamataAjax(messaggio, stringa_movie, tipoFilm);
@@ -210,27 +206,25 @@ $(document).ready(function () {
   });
 
   //per associare  la pressione del tasto enter nella textarea al bottone
-  $("#input-text").keydown(function (event) {
+  $('#input-text').keydown(function (event) {
     if (event.keyCode === 13) {
-      $("#input-button").click();
+      $('#input-button').click();
     }
   });
 
   //plugin che flippa le card
-  $(".film-container").on("mouseenter", ".film-card", function () {
-    $(".flip").flip({
-      trigger: "hover",
+  $('.film-container').on('mouseenter', '.film-card', function () {
+    $('.flip').flip({
+      trigger: 'hover',
     });
   });
+});
 
-  //se il titolo è uguale al titolo originale, nascondo il titolo originale
-  $(".film-container").on("mouseenter", ".film-card", function () {
-    var titolo = $(".title", this).html();
-    console.log(titolo);
-    var titolo_originale = $(".original-title", this).html();
-    console.log(titolo_originale);
-    if (titolo == titolo_originale) {
-      $(".original-title", this).addClass("hide");
+//se il titolo è uguale al titolo originale, nascondo il titolo originale
+$(document).ajaxComplete(function () {
+  $('.film-card').each(function () {
+    if ($('.title', this).html() === $('.original-title', this).html()) {
+      $('.original-title', this).addClass('hide');
     }
   });
 });
